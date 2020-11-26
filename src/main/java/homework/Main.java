d8e681ad5996bf30070349adb1e414cb55495df6
Nastia Fasova <fasovaaa@gmail.com>
2020-11-26 19:10:07 +0200

    Added new classes

---
+++ src/main/java/homework/Main.java
@@ -0,0 +1,8 @@
+package homework;
+
+public class Main {
+    public static void main(String[] args) {
+        StringPractice practice = new StringPractice();
+        practice.displayOutput(args);
+    }
+}
