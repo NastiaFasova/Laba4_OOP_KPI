cdd08eabb715ae54ec5a6b0a8bb7dc24805bb835
Nastia Fasova <fasovaaa@gmail.com>
2020-11-27 10:45:30 +0200

    Changed pattern

--- src/main/java/homework/StringPractice.java
+++ src/main/java/homework/StringPractice.java
@@ -6,7 +6,7 @@ import java.util.regex.Pattern;
 
 public class StringPractice {
     private String[] readString(String[] strings) {
-        String regex = "[(\\p{L}|\\p{C}]+";
+        String regex = "^[а-яА-ЯёЁa-zA-Z0-9]+$";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(String.join(" ", strings));
         if (!matcher.find()) {
